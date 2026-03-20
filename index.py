cd26a0c6cd35b2ab0b89e28673938c3732ca20b5
yr.solo <yr.solo@gmail.com>
2026-03-20 18:09:18 +0300

    refactor: inline trivial entrypoint shell getters

--- index.py
+++ index.py
@@ -15,26 +15,14 @@ def _get_app_context():
     return get_app_context()
 
 
-def _get_http_shell():
-    return get_http_shell()
-
-
-def _get_worker_shell():
-    return get_worker_shell()
-
-
-def _get_trigger_shell():
-    return get_trigger_shell()
-
-
 async def handler(event, _):
     """Yandex Cloud handler."""
     return await handle_entrypoint(
         event,
         _,
-        get_http_shell=_get_http_shell,
-        get_worker_shell=_get_worker_shell,
-        get_trigger_shell=_get_trigger_shell,
+        get_http_shell=get_http_shell,
+        get_worker_shell=get_worker_shell,
+        get_trigger_shell=get_trigger_shell,
         triggers=APP_TRIGGERS,
         telegram_webhook_path=_get_app_context().cfg.runtime.telegram.webhook_path,
     )
